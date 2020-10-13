bac424b211fb253680b46e36bf23a8f6dec55be7
dergaxhtml <Aholdwbk93!>
2020-10-13 22:18:28 +0200

    not work yet

--- src/torun_places/android/core.cljs
+++ src/torun_places/android/core.cljs
@@ -1,25 +1,16 @@
 (ns torun-places.android.core
   (:require [reagent.core :as r :refer [atom]]
-            [re-frame.core :refer [subscribe dispatch dispatch-sync]]
-            [torun-places.events]
+            [re-frame.core :refer [dispatch dispatch-sync]]
+            [torun-places.events refer [load-views]]
+            [torun-places.root]
             [torun-places.subs]))
 
-(def ReactNative (js/require "react-native"))
-(def Animatable (js/require "react-native-animatable"))
 
+
+(def ReactNative (js/require "react-native"))
 
 (def app-registry (.-AppRegistry ReactNative))
-(def text (r/adapt-react-class (.-Text ReactNative)))
-(def view (r/adapt-react-class (.-View ReactNative)))
-(def image (r/adapt-react-class (.-Image ReactNative)))
-(def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
-(def ListView (.-ListView ReactNative))
 
-
-(def list-view (r/adapt-react-class (.-ListView ReactNative)))
-
-
-(def logo-img (js/require "./images/cljs.png"))
 
 (defn alert [title]
       (.alert (.-Alert ReactNative) title))
